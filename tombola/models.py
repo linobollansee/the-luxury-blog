fa189fadede92d256dc7bc5234fb307d72f1e74e
Lino Bollansee <linobollansee@hotmail.com>
2024-10-25 02:02:57 +0000

    Update tombola models with unique email

--- tombola/models.py
+++ tombola/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Participant(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
