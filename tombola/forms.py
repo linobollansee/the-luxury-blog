0a0dbadff4662fea0326e9d6a606d3379f7c9bc3
Lino Bollansee <linobollansee@hotmail.com>
2024-10-25 02:21:22 +0000

    Comment tombola forms

--- tombola/forms.py
+++ tombola/forms.py
@@ -1,7 +1,22 @@
-from django import forms
-from .models import Participant
+# Import the necessary classes from Django forms and the Participant model
+from django import forms  # Importing Django's forms module for creating forms
+from .models import Participant  # Importing the Participant model
+
 
 class ParticipantForm(forms.ModelForm):
+    """
+    A form for creating and updating Participant instances.
+
+    This form is based on the Participant model and includes fields
+    for participant's name and email.
+    """
+
     class Meta:
+        """
+        Meta class to define additional properties for the form.
+        """
+        # Specify the model that this form is associated with
         model = Participant
-        fields = ['name', 'email']
+     
+        # Define the fields to be included in the form
+        fields = ['name', 'email']  # Only the fields will be rendered.
