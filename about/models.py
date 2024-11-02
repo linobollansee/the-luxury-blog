d6d02dbf7b2467cfbd7f7680c5d7fb7c40cb3a73
Lino Bollansee <linobollansee@hotmail.com>
2024-11-02 16:18:59 +0000

    Comment about/models.py

--- about/models.py
+++ about/models.py
@@ -10,6 +10,7 @@ class About(models.Model):
     """
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
+    # Added profile_image2 variable and field type
     profile_image2 = CloudinaryField('image2', default='placeholder2')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -23,6 +24,7 @@ class CollaborateRequest(models.Model):
     Stores a single collaboration request message
     """
     name = models.CharField(max_length=200)
+    # Added luxury_category variable and field type
     luxury_category = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
